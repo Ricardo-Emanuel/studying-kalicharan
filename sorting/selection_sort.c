826013f1198685aed615fcfe6293c629110455b7
Ricardo Emanuel <ricardoemanuelde@gmail.com>
2023-03-07 14:44:20 -0300

    :construction: Initialized the sorting folder

---
+++ sorting/selection_sort.c
@@ -0,0 +1,50 @@
+# include <stdio.h>
+
+# define MaxNumbers 10
+
+void swap(int list[], int i, int j) {
+    int temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+}
+
+int getSmallest(int list[], int lo, int hi) {
+    int small = lo;
+    for (int h = lo + 1; h <= hi; h++) {
+        if (list[h] < list[small]) small = h;
+    }
+    return small;
+}
+
+void selectionSort(int list[], int lo, int hi) {
+    // The functions are initialized inside selectionSort
+    // Because they are only used here
+    int getSmallest(int[], int, int);
+    void swap(int[], int, int);
+    for (int h = lo; h < hi; h++) {
+        int s = getSmallest(list, h, hi);
+        swap(list, h, s);
+    }
+}
+
+int main() {
+    void selectionSort(int[], int, int);
+    int num[MaxNumbers];
+    printf("Type up to %d numbers followed by 0\n", MaxNumbers);
+    int n = 0, v;
+    scanf("%d", &v);
+    while (v != 0 && n < MaxNumbers) {
+        num[n++] = v;
+        scanf("%d", &v);
+    }
+    if (v != 0) {
+        printf("More than %d numbers entered\n", MaxNumbers);
+        printf("First %d used\n", MaxNumbers);
+    }
+    selectionSort(num, 0, n-1);
+    printf("\nThe sorted numbers are\n");
+    for (int h = 0; h < n; h++) printf("%d ", num[h]);
+    printf("\n");
+
+    return 0;
+}
